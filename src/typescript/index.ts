9eb8ed9d2f13bffcac1f6e9ed137e63909edbf06
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-20 23:19:36 -0300

    add basic SASS structure, useClamp and useMedia hooks

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,1 +1,11 @@
-console.log("ho")
+if("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const register = await navigator.serviceWorker.register("./pwa/serviceWorker.js")
+      
+      console.log(register)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+}
