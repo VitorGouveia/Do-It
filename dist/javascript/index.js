c723c2f893f96987f475efd23dfccebf275aaa60
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-11-20 23:19:36 -0300

    add basic SASS structure, useClamp and useMedia hooks

---
+++ dist/javascript/index.js
@@ -0,0 +1,12 @@
+"use strict";
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", async () => {
+        try {
+            const register = await navigator.serviceWorker.register("./pwa/serviceWorker.js");
+            console.log(register);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    });
+}
---
+++ src/typescript/pwa/serviceWorker.ts
@@ -0,0 +1,31 @@
+self.addEventListener("install", () => {
+  console.log("installing")
+})
+
+self.addEventListener("activate", () => {
+  console.log("activating")
+})
+
+self.addEventListener("fetch", () => {
+  console.log("fetching")
+})
+
+window.addEventListener("beforeinstallprompt", e => {
+  console.log("before install")
+  e.preventDefault()
+})
+
+navigator.serviceWorker.ready.then(registration => {
+  if(!registration.pushManager) {
+    console.log("no notifications support")
+    return false
+  }
+
+  registration.pushManager.subscribe({
+    userVisibleOnly: true
+  }).then(subscription => {
+    console.log("subscribed")
+  }).catch(error => {
+    console.log("error in subscription", error)
+  })
+})
