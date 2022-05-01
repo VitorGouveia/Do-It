928d1010c7e57c036bd00c20074e5b22d6f29870
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-05-01 10:51:00 -0300

    create deploy script for both apps

--- apps/v2/next.config.js
+++ apps/v2/next.config.js
@@ -1,6 +1,13 @@
+const __isGithub__ = process.env.GH_PAGES === "true";
+
+const projectName = "Notely";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  basePath: __isGithub__ ? `/${projectName}` : "",
+  assetPrefix: __isGithub__ ? `/${projectName}/` : "",
+};
+
+module.exports = nextConfig;
